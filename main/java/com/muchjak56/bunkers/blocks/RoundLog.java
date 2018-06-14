7cc86094767574a0e86afcd91d6dee07664a3f78
Muchjak56 <37027978+Muchjak56@users.noreply.github.com>
2018-06-13 22:37:32 -0400

    added tool function
    added function to  ultimum axe to chop stockade tops in world

--- main/java/com/muchjak56/bunkers/blocks/RoundLog.java
+++ main/java/com/muchjak56/bunkers/blocks/RoundLog.java
@@ -2,17 +2,25 @@ package com.muchjak56.bunkers.blocks;
 
 import javax.annotation.Nullable;
 
+import com.google.common.base.Predicate;
 import com.muchjak56.bunkers.Items.tools.ToolAxe;
 import com.muchjak56.bunkers.blocks.blocktype.PillarType;
 import com.muchjak56.bunkers.blocks.blocktype.TorchType;
+import com.muchjak56.bunkers.commands.util.Teleport;
 import com.muchjak56.bunkers.init.ModBlocks;
 import com.muchjak56.bunkers.init.ModItems;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -22,7 +30,8 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class RoundLog extends PillarType {
-
+	
+	
 	public RoundLog(String name, Material material) 
 	{
 		super(name, material);
@@ -36,6 +45,9 @@ public class RoundLog extends PillarType {
 		//setBlockUnbreakable();
 		this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS, EnumFacing.Axis.Y));
 	}
+	
+	
+	
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
